f7a8e2ba721e5a1043c72c308be702c9ef623777
Erik Sauerbier <erik.sauerbier@mailbox.org>
2022-05-23 08:20:54 +0200

    v.1.1
    - Skript Kommentare hinzugefügt
    - Skript Layout opitmiert

---
+++ Name der ClusterVMs ausgeben.ps1
@@ -0,0 +1,17 @@
+﻿# Skript zum Ausgeben der vorhandenen Cluster VMs
+# Stannek GmbH - v.1.1 - 12.05.2022 - E.Sauerbier
+
+# Parameter
+$FileOutputName = "Name Cluster-VMs.csv"
+
+$PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$FileOutput = $PSScriptRoot + "\" + $FileOutputName
+
+# Alle Cluster VMs auslesen und in CSV exportieren
+Get-ClusterResource -Cluster RZCLuster | Where ResourceType -eq "Virtual Machine" | Select OwnerGroup | Export-Csv -path $FileOutput
+
+# Alle Cluster VMs auslesen und in Shell ausgeben
+Get-ClusterResource -Cluster RZCLuster | Where ResourceType -eq "Virtual Machine" | Select OwnerGroup, Name
+
+# Aufforderung zum beenden des Skripts
+Read-Host "`nZum beenden beliebige Taste drücken"

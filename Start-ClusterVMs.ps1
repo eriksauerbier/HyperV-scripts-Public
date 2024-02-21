4e72d9b79789de843c3fa04f850efc98cb4f664e
eriksauerbier <erik.sauerbier@mailbox.org>
2024-02-21 13:15:30 +0100

    v.1.0
    - First Release

---
+++ Start-ClusterVMs.ps1
@@ -0,0 +1,25 @@
+﻿# Skript zum starten von Cluster VMs nach einen Neustart des Hosts
+# Stannek GmbH - v.1.0 - 07.02.2024 - E.Sauerbier
+
+# Parameter
+$NameCRDC = 'Virtueller Computer "DC01"'
+$NameCRFS = 'Virtueller Computer "FS01"'
+
+# Starte DC, wenn noch nicht gestartet
+If ((Get-ClusterResource -Name $NameCRDC).State -eq "Offline") {Start-ClusterResource -Name $NameCRDC}
+
+# Warte einen Moment
+Start-Sleep -Seconds 120
+
+# Starte FS, wenn noch nicht gestartet
+If ((Get-ClusterResource -Name $NameCRFS).State -eq "Offline") {Start-ClusterResource -Name $NameCRFS}
+
+# Warte einen Moment
+Start-Sleep -Seconds 120
+
+# Starte die Restlichen VMs
+$OfflineVMs = Get-ClusterResource | Where-Object {($_.ResourceType -EQ "Virtual Machine") -and ($_.State -eq "Offline")}
+foreach ($OfflineVM in $OfflineVMs) {
+                Start-ClusterResource -Name $OfflineVM.Name
+                Start-Sleep 10
+                }
---
+++ Start-HyperV-VMs.ps1
@@ -0,0 +1,25 @@
+﻿# Skript zum starten von Hyper-V VMs nach einen Neustart des Hosts
+# Stannek GmbH - v.1.0 - 07.02.2024 - E.Sauerbier
+
+# Parameter
+$NameCRDC = 'DC01'
+$NameCRFS = 'FS01'
+
+# Starte DC, wenn noch nicht gestartet
+If ((Get-VM -Name $NameCRDC -ErrorAction SilentlyContinue).State -eq "Off") {Start-VM -Name $NameCRDC}
+
+# Warte einen Moment
+Start-Sleep -Seconds 120
+
+# Starte FS, wenn noch nicht gestartet
+If ((Get-VM -Name $NameCRFS -ErrorAction SilentlyContinue).State -eq "Off") {Start-VM -Name $NameCRFS}
+
+# Warte einen Moment
+Start-Sleep -Seconds 60
+
+# Starte die Restlichen VMs
+$OfflineVMs = Get-VM | Where-Object {$_.State -eq "Off"}
+foreach ($OfflineVM in $OfflineVMs) {
+                Start-VM -Name $OfflineVM.Name
+                Start-Sleep 10
+                }

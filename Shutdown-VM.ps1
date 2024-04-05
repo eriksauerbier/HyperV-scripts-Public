499c67bd3e047b760cfdc2669003d0941c9f4c3d
eriksauerbier <erik.sauerbier@mailbox.org>
2024-04-05 13:38:36 +0200

    v.1.1
    - Skriptlayout optimiert
    - Shutdown des Hosts hinzugefügt

--- Shutdown-VM.ps1
+++ Shutdown-VM.ps1
@@ -1,8 +1,17 @@
-# Skript zum herunterfahren von VMs
-# Stannek GmbH - v.1.0 - 04.04.2024 - E.Sauerbier
+# Skript zum herunterfahren von VMs und Host durch USV
+# Stannek GmbH - v.1.1 - 05.04.2024 - E.Sauerbier
 
+# laufenden VMs auslesen
 $RunningVMs = Get-VM | Where State -eq "Running"
+
+# einzelne VM herunterfahren
 foreach ($RunningVM in $RunningVMs) {
             Write-Host "VMs auf diesen Host werden heruntergefahren"
             Stop-vm -Name $RunningVM.Name
             }
+
+# 1 Minute warten
+Start-Sleep -Seconds 60
+
+# Host herunterfahren
+Stop-Computer -ComputerName Localhost
